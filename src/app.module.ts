a5a8b168bf6fb0809fdcb1173b6f7dec61972a28
Ivan Herrera <ivan.herrera@omni.pro>
2022-12-30 10:15:21 -0500

    post restApi v1

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://root:example@localhost:27017/', { useNewUrlParser: true }),
+    BlogModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
